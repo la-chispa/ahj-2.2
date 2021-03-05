6a2ab1c65c974f39f244ad2e27044ccf65ea3d0b
Olga Belotserkovskaya <olla.chispa@gmail.com>
2021-03-06 03:37:54 +0600

    fix

--- src/index.js
+++ src/index.js
@@ -2,4 +2,18 @@ import './css/style.css';
 
 import './js/app';
 
+import './js/data';
+
+import './js/less';
+
+import './js/more';
+
+import './js/parse';
+
+import './js/render';
+
+import './js/select';
+
+import './js/sort';
+
 // TODO: write your code in app.js
